d05469455819019326bc4c6ace46fc78cee43e5c
kaiqnes <caique.nunes@mercadolivre.com>
2021-04-23 01:04:04 -0300

    Adjust parameters to recognize message origin

--- animated_chat_window.js
+++ animated_chat_window.js
@@ -1,12 +1,12 @@
 (function() {
     var Message;
     Message = function(arg) {
-        this.text = arg.text, this.message_side = arg.message_side;
+        this.text = arg.text, this.origin = arg.origin;
         this.draw = function(_this) {
             return function() {
                 var $message;
                 $message = $($('.message_template').clone().html());
-                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $message.addClass(_this.origin).find('.text').html(_this.text);
                 $('.messages').append($message);
                 return setTimeout(function() {
                     return $message.addClass('appeared');
@@ -16,42 +16,40 @@
         return this;
     };
     $(function() {
-        var getMessageText, message_side, sendMessage;
-        message_side = 'right';
+        var getMessageText, sendMessage;
         getMessageText = function() {
             var $message_input;
             $message_input = $('.message_input');
             return $message_input.val();
         };
-        sendMessage = function(text) {
+        sendMessage = function(origin, text) {
             var $messages, message;
             if (text.trim() === '') {
                 return;
             }
             $('.message_input').val('');
             $messages = $('.messages');
-            message_side = message_side === 'left' ? 'right' : 'left';
             message = new Message({
                 text: text,
-                message_side: message_side
+                origin: origin.toLowerCase()
             });
             message.draw();
             return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
         };
         $('.send_message').click(function(e) {
-            return sendMessage(getMessageText());
+            return sendMessage('USER', getMessageText());
         });
         $('.message_input').keyup(function(e) {
             if (e.which === 13) {
-                return sendMessage(getMessageText());
+                return sendMessage('USER', getMessageText());
             }
         });
-        sendMessage('Hello Philip! :)');
+        sendMessage('BOT', 'Hello Philip! :)');
         setTimeout(function() {
-            return sendMessage('Hi Sandy! How are you?');
+            return sendMessage('USER', 'Hi Sandy! How are you?');
         }, 1000);
         return setTimeout(function() {
-            return sendMessage('I\'m fine, thank you!');
+            return sendMessage('BOT', 'I\'m fine, thank you!');
         }, 2000);
     });
 }.call(this));
